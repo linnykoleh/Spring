be5360797e009bb0598db203e544b01779c1b7a0
LinnykOleh <linnik.oleg.93@gmail.com>
2018-08-23 16:04:56 +0200

    added look up method injection into the project

---
+++ LMI/src/main/java/com/lmi/components/Message.java
@@ -0,0 +1,43 @@
+package com.lmi.components;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class Message {
+
+	private String target;
+	private String destination;
+	private String body;
+
+	public void setTarget(String target) {
+		this.target = target;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public String getTarget() {
+		return target;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	void read(){
+		System.out.println(hashCode());
+	}
+
+
+}
