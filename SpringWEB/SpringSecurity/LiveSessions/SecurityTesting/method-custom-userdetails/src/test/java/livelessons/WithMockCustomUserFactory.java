fb3e8b3bcf1cd5273f863f829ffadbca7a2ef19a
LinnykOleh <linnik.oleg.93@gmail.com>
2019-01-30 22:32:42 +0200

    finished spring security live lessons

---
+++ SpringWEB/SpringSecurity/LiveSessions/SecurityTesting/method-custom-userdetails/src/test/java/livelessons/WithMockCustomUserFactory.java
@@ -0,0 +1,38 @@
+package livelessons;
+
+import livelessons.user.MessageUser;
+import livelessons.user.UserRepository;
+import livelessons.user.UserRepositoryUserDetailsService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class WithMockCustomUserFactory
+		implements WithSecurityContextFactory<WithMockMessageUser> {
+
+	@Override
+	public SecurityContext createSecurityContext(WithMockMessageUser annotation) {
+		UserRepository users = mock(UserRepository.class);
+		when(users.findByEmail(any())).thenReturn(createUser(annotation));
+
+		UserRepositoryUserDetailsService userDetailsService = new UserRepositoryUserDetailsService(
+				users);
+		UserDetails principal = userDetailsService.loadUserByUsername(annotation.value());
+
+		SecurityContext context = SecurityContextHolder.createEmptyContext();
+		context.setAuthentication(new UsernamePasswordAuthenticationToken(principal,
+				principal.getPassword(), principal.getAuthorities()));
+		return context;
+	}
+
+	private MessageUser createUser(WithMockMessageUser annotation) {
+		return new MessageUser(annotation.id(), annotation.value(), "notused");
+	}
+
+}
---
+++ SpringWEB/SpringSecurity/LiveSessions/SecurityTesting/method-custom-userdetails/src/test/java/livelessons/WithMockMessageUser.java
@@ -0,0 +1,21 @@
+package livelessons;
+
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@WithSecurityContext(factory = WithMockCustomUserFactory.class)
+public @interface WithMockMessageUser {
+
+	long id() default 1L;
+
+	String value() default "user@example.com";
+
+}
