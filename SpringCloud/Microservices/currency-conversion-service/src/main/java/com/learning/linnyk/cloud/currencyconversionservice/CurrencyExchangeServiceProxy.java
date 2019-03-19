a932dad36d05dc625ec51d264fd87dec0163a495
LinnykOleh <linnik.oleg.93@gmail.com>
2019-03-19 11:41:27 +0100

    added some notes about spring cloud

--- SpringCloud/Microservices/currency-conversion-service/src/main/java/com/learning/linnyk/cloud/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ SpringCloud/Microservices/currency-conversion-service/src/main/java/com/learning/linnyk/cloud/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name = "currency-exchange-service")
-@FeignClient(name = "netflix-zuul-api-gateway-server") // 1. Uses in order not to call microservice via api-gateway
-@RibbonClient(name = "currency-exchange-service") // 2. Ribbon use Discovery Server to find needed microservice and balance requests
+@FeignClient(name = "netflix-zuul-api-gateway-server") // 1.
+@RibbonClient(name = "currency-exchange-service")      // 2.
 public interface CurrencyExchangeServiceProxy {
 
-//    @GetMapping("/currency-exchange/from/{from}/to/{to}") // 1. Uses in order not to call microservice via api-gateway
-    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
+    // 1. Uses in order not to call microservice via api-gateway
+    // 2. Ribbon use Discovery Server to find needed microservice and balance requests
+
+    /*@GetMapping("/currency-exchange/from/{from}/to/{to}") */                      // 1.
+    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}") // 2.
     CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 
 }
